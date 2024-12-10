39a285940840e59e21842c8a139e9fb31a86670f
AnderG2003 <agorocica002@ikasle.ehu.eus>
2024-12-10 12:42:04 +0100

    Ejer2

---
+++ ejer2.py
@@ -0,0 +1,9 @@
+def suma(num1, num2):
+    suma = num1 + num2
+    return suma
+
+if __name__ == "__main__":
+    num1 = 2
+    num2 = 3
+    suma = suma(num1,num2)
+    print("La suma de los num es: ", suma)   
